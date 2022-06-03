51a690be43a51aeab84667adae883126e7280862
gregsaville <gregsaville767@gmail.com>
2022-06-03 13:49:07 -0500

    cleaned up.

--- src/main/kotlin/com/bushelpowered/pokedex/controller/PokemonController.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/controller/PokemonController.kt
@@ -2,7 +2,6 @@ package com.bushelpowered.pokedex.controller
 
 import com.bushelpowered.pokedex.service.PokemonService
 import com.bushelpowered.pokedex.entities.Pokemon
-import org.springframework.context.annotation.Primary
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
--- src/main/kotlin/com/bushelpowered/pokedex/entities/Trainer.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/entities/Trainer.kt
@@ -1,14 +1,8 @@
 package com.bushelpowered.pokedex.entities
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.JsonRawValue
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
-import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
 @Table(name = "trainers")
