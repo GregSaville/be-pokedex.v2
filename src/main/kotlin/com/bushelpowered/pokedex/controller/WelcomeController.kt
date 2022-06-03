626d9f31dd0eb71a4fa209f1b6b8dad1cd39a323
gregsaville <gregsaville767@gmail.com>
2022-06-03 10:52:22 -0500

    cleaned up capturedPokemon field

--- src/main/kotlin/com/bushelpowered/pokedex/controller/WelcomeController.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/controller/WelcomeController.kt
@@ -5,11 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
+
 class WelcomeController {
 
-    @GetMapping("")
+    val welcomeHTML = "<h1>Welcome to the Pokedex API</h1>" +
+            " <h2>---Public Endpoints---</h2> <h4>/welcome</h4> <h4>/api/pokemon</h4>   <h4>/api/pokemon/{pokemon id}</h4> <h2>---Authenticated Endpoints---</h2> <h4>/api/trainers</h4>  <h4>/api/trainers/{trainer id}</h4>  <h4>/api/trainers/{trainers id}/captured</h4>" +
+            "<h3>Add Trainer -> @POST /api/trainers email=\"string\" & password=\"string\" </h3>" +
+            "<h3>Mark Pokemon as Captued -> @PUT /api/trainers/{trainers id}  Pokemon's number </h3>" +
+            "<h4>-----> can add multiple pokemon in one put request format like  4 5 1 356 291  </h4>" +
+            "<h3>Clear Trainers Pokemon  -> @DELETE /api/trainers/{trainers id}/captured</h3>"
+
+    @GetMapping("/welcome")
     fun welcome() : String{
-        return "~Welcome to the Pokedex API~  @GetMethods /pokemon   /pokemon/{pokemon id}  /trainers  /trainers/{trainer id}  /trainers/{trainers id}/captured"
+        return welcomeHTML
     }
+
+    @GetMapping("/api")
+    fun reroute() : String{
+        return welcome()
+    }
+
 }
--- src/main/kotlin/com/bushelpowered/pokedex/service/TrainerService.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/service/TrainerService.kt
@@ -5,7 +5,6 @@ import com.bushelpowered.pokedex.entities.Trainer
 import com.bushelpowered.pokedex.repositories.PokemonRepository
 import com.bushelpowered.pokedex.repositories.TrainerRepository
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.Optional
 
@@ -26,7 +25,7 @@ class TrainerService(val trainerDB: TrainerRepository, db: PokemonRepository) : 
                 Trainer(
                 email = trainer.get().email,
                 password = trainer.get().password,
-                capturedPokemon = trainer.get().capturedPokemon + " " + pokeId,
+                capturedPokemon = handleCapture(trainer.get().capturedPokemon, pokeId),
                 id = trainer.get().id
             )
             )
@@ -54,6 +53,52 @@ class TrainerService(val trainerDB: TrainerRepository, db: PokemonRepository) : 
             )
         }
 
+    }
+
+    private fun verifyNumber(toTest: String): String {
+        var testList = toTest.split(" ")
+        testList = testList.toSet().toList()
+        var returnString = ""
+        testList.forEach{
+            if(isNumber(it)){
+                if(it.toInt() in 1..553){
+                        returnString = "$returnString $it"
+                }else{
+                    println("Invalid Pokemon Number: $it")
+                }
+            }else{
+                println("Invalid Pokemon Number: $it")
+            }
+        }
+        return returnString.trim()
+    }
+
+    private fun isNumber(token : String) : Boolean{
+        return if(token.isNullOrEmpty()){
+            false
+        } else token.all { Character.isDigit(it) }
+    }
+
+    private fun handleCapture(capturedPokemon: String, pokeId: String): String {
+        var updatedCapturedPokemon = if(capturedPokemon.isEmpty()){
+            verifyNumber(pokeId)
+        } else {
+            capturedPokemon + " " + verifyNumber(pokeId)
+        }
+        updatedCapturedPokemon = removeDuplicatesAndSort(updatedCapturedPokemon.split(" "))
+        return updatedCapturedPokemon
+    }
+
+    private fun removeDuplicatesAndSort(updatedCapturedPokemon: List<String>): String {
+        var returnString = ""
+        var sortedCapturedPokemon = mutableListOf<Int>()
+        updatedCapturedPokemon.toSet().toList().forEach{
+            sortedCapturedPokemon.add(it.toInt())
+        }
+        sortedCapturedPokemon.sorted().forEach{
+            returnString = "$returnString${it.toString()} "
+        }
+        return returnString.trim()
     }
 }
 
